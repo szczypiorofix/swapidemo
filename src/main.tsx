025a1f1d28f60b174db2176814f2ef0a2e750896
szczypiorofix <szczypiorofix@o2.pl>
2025-02-16 21:18:37 +0100

    Add basic theme

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import './index.css';
+import { CssBaseline, ThemeProvider } from '@mui/material';
+
 import App from './App.tsx';
+import theme from './theme.tsx';
+
+import './index.css';
+import { StrictMode } from 'react';
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
-        <App />
+        <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <App />
+        </ThemeProvider>
     </StrictMode>
 );
